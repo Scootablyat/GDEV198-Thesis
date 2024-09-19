a312963623826f71528df46505491bf23e405bab
JT <johnterra1215@gmail.com>
2024-09-19 11:38:14 +0800

    added select structure mechanic, addeed base code for UI (command bar, buttons, etc.), added basic code to set up for prefab spawning

--- GDEV_Thesis_RTS (2_5D)/Assets/Scripts/StructureScritps/Barracks.cs
+++ GDEV_Thesis_RTS (2_5D)/Assets/Scripts/StructureScritps/Barracks.cs
@@ -7,9 +7,18 @@ public class Barracks : MonoBehaviour
     // Start is called before the first frame update
     public GameObject barracksObj;
     public GameObject commandBarObj;
+    [SerializeField]
+    private List<GameObject> trainableUnitsList;
+    [SerializeField]
+    private List<GameObject> commandBarButtons;
     void Start()
     {
         barracksObj = this.gameObject;
+        commandBarButtons = commandBarObj.GetComponent<CommandBar>().getAllCommandButtons();
+    }
+
+    void LoadUI(){
+        
     }
 
     // Update is called once per frame
